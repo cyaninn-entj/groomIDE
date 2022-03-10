337421b11d96ece6bdf28b7f7dac651fa3230dae
성연우 <baceball@nate.com>
2022-03-10 07:58:51 +0000

    baekjoon online judge problem no.10870

--- baekjoon_online_judge/step10/10870.py
+++ baekjoon_online_judge/step10/10870.py
@@ -1,16 +1,15 @@
 def FN(num) :
-    val=0
+    i=num
     if len(fn_ls)-1>=num :
         return fn_ls[num]
     else :
-        val=sum(fn_ls[-2:])
-        fn_ls.append(val)
-        FN(num)
+        fn_ls.append(fn_ls[-1]+fn_ls[-2])
+        return FN(i)
 
 n=int(input())
 fn_ls=[0,1]
 
-print(fn_ls)
 print(FN(n))
+#print(fn_ls)
 
     
